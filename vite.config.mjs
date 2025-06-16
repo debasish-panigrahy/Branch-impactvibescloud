4b510f2b98c7aa576e38ac177a1addddc463b3ac
debasish-panigrahy <panigrahydebasish4@gmail.com>
2025-06-16 17:51:15 +0530

    build commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,3 +1,55 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import path from 'node:path'
+// import autoprefixer from 'autoprefixer'
+
+// export default defineConfig(() => {
+//   return {
+//     base: './',
+//     build: {
+//       outDir: 'dist',
+//     },
+//     publicDir: 'public',
+//     css: {
+//       postcss: {
+//         plugins: [
+//           autoprefixer({}), // add options if needed
+//         ],
+//       },
+//     },
+//     esbuild: {
+//       loader: 'jsx',
+//       include: /src\/.*\.jsx?$/,
+//       exclude: [],
+//     },
+//     optimizeDeps: {
+//       force: true,
+//       esbuildOptions: {
+//         loader: {
+//           '.js': 'jsx',
+//         },
+//       },
+//     },
+//     plugins: [react()],
+//     resolve: {
+//       alias: [
+//         {
+//           find: 'src/',
+//           replacement: `${path.resolve(__dirname, 'src')}/`,
+//         },
+//       ],
+//       extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.scss'],
+//     },
+//     server: {
+//       host: '0.0.0.0',
+//       port: process.env.PORT || 5030,
+//       allowedHosts: 'all'
+//     },
+//   }
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
@@ -5,15 +57,15 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/', // ✅ Use `/` instead of `./` for correct routing on Vercel
     build: {
-      outDir: 'build',
+      outDir: 'dist', // ✅ Default Vercel expects
     },
     publicDir: 'public',
     css: {
       postcss: {
         plugins: [
-          autoprefixer({}), // add options if needed
+          autoprefixer(),
         ],
       },
     },
